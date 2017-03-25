6d0513ea76720a016dd88a5a82054ef9d0ca5003
wangoasis <wangqingsky@outlook.com>
2017-03-25 03:57:24 -0400

    Initial Commit

---
+++ Hack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hack
+//
+//  Created by Qing Wang on 3/25/17.
+//  Copyright © 2017 Qing Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
